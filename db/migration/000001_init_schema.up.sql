8c76f8744510d6b5540811a98e2a39ec060c3f1b
mxq7854687 <mxq7854687@gmail.com>
2022-05-09 02:47:45 +0800

    fix unit test null manager id

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -10,18 +10,27 @@ CREATE TABLE jobs (
 	max_salary bigserial
 );
 
-CREATE TABLE employees (
-	employee_id bigserial PRIMARY KEY,
-	first_name CHARACTER VARYING (20),
-	last_name CHARACTER VARYING (25) NOT NULL,
-	email CHARACTER VARYING (100) NOT NULL,
-	phone_number CHARACTER VARYING (20),
-	hire_date DATE NOT NULL,
-	job_id bigserial NOT NULL,
-	salary bigserial NOT NULL,
-	manager_id bigserial,
-	department_id bigserial,
-	FOREIGN KEY (job_id) REFERENCES jobs (job_id) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (department_id) REFERENCES departments (department_id) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (manager_id) REFERENCES employees (employee_id) ON UPDATE CASCADE ON DELETE CASCADE
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS employees_employee_id_seq;
+CREATE SEQUENCE IF NOT EXISTS employees_job_id_seq;
+CREATE SEQUENCE IF NOT EXISTS employees_salary_seq;
+CREATE SEQUENCE IF NOT EXISTS employees_manager_id_seq;
+CREATE SEQUENCE IF NOT EXISTS employees_department_id_seq;
+
+-- Table Definition
+CREATE TABLE "public"."employees" (
+    "employee_id" int8 NOT NULL DEFAULT nextval('employees_employee_id_seq'::regclass),
+    "first_name" varchar(20),
+    "last_name" varchar(25) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    "phone_number" varchar(20),
+    "hire_date" date NOT NULL,
+    "job_id" int8 NOT NULL DEFAULT nextval('employees_job_id_seq'::regclass),
+    "salary" int8 NOT NULL DEFAULT nextval('employees_salary_seq'::regclass),
+    "manager_id" int8 DEFAULT nextval('employees_manager_id_seq'::regclass),
+    "department_id" int8 NOT NULL DEFAULT nextval('employees_department_id_seq'::regclass),
+    CONSTRAINT "employees_department_id_fkey" FOREIGN KEY ("department_id") REFERENCES "public"."departments"("department_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "employees_job_id_fkey" FOREIGN KEY ("job_id") REFERENCES "public"."jobs"("job_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "employees_manager_id_fkey" FOREIGN KEY ("manager_id") REFERENCES "public"."employees"("employee_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY ("employee_id")
 );
