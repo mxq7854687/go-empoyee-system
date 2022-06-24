7a39fa01b56e0be10039d0bfecaa983815c04e84
mxq7854687 <mxq7854687@gmail.com>
2022-06-24 21:41:25 +0800

    add test to acitivate user endpoint

--- api/main_test.go
+++ api/main_test.go
@@ -3,9 +3,11 @@ package api
 import (
 	"os"
 	"testing"
+
+	"github.com/gin-gonic/gin"
 )
 
 func TestMain(m *testing.M) {
-	// gin.SetMode(gin.TestMode)
+	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
 }
